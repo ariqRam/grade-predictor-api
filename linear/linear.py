65e162f045c91dfb1f3225cbfbe0f275b3a85ed3
ramdhanyA <ariqathallah38@gmail.com>
2023-01-17 11:02:36 +0800

    Added all the files

---
+++ linear/linear.py
@@ -0,0 +1,14 @@
+import pickle
+import numpy as np
+
+def linear_predict(data):
+    # data is a 2d numpy array
+    model = pickle.load(open('linear/linear_model.sav', 'rb'))
+
+    pipeline = pickle.load(open('linear/standard_scaler.sav', 'rb'))
+    
+    data = pipeline.transform(data)
+    
+    pred = model.predict(data)
+
+    return pred
